9fbf263a62b19bf171b46a01fbfb59f53066ba48
oz0820 <85290191+oz0820@users.noreply.github.com>
2023-12-13 14:37:22 +0900

    avatarのオリジナル画像を取得するURLを変更し，常に取得するように戻した

--- YT-Membership-exporter.js
+++ YT-Membership-exporter.js
@@ -1,10 +1,10 @@
 javascript:
 /*
-APP_VERSION: 2023.12.13.1
+APP_VERSION: 2023.12.13.2
 Github_Rep: https://github.com/oz0820/YT-Membership-exporter
 */
 (async function(){
-const APP_VERSION = '2023.12.13.1'
+const APP_VERSION = '2023.12.13.2'
 
 // 外部のライブラリ読み込み
 const importInNoModule = (url) => new Promise(resolve => {
@@ -268,19 +268,14 @@ try {
 
     const avatar_elm = document.querySelector('div#channel-container.ytd-c4-tabbed-header-renderer yt-img-shadow img')
     const avatar_url = avatar_elm.src
+    const avatar_original_url = avatar_url.split('=')[0]
 
     channel_image_urls = {
         'banner': banner_url,
         'banner_original': banner_og_url,
         'avatar': avatar_url,
+        'avatar_original': avatar_original_url
     }
-
-    // ユーザーが設定したアバターの場合だけ保存する
-    // 自動生成の場合は，pathが /ytc/ から始まる
-    if (!(new URL(avatar_url).pathname.startsWith('/ytc/'))) {
-        channel_image_urls['avatar_original'] = avatar_url.split('=')[0] + '=w0'
-    }
-
 } catch (e) {
     logger.error(e)
     if (!confirm('バナー・アバター画像を検出できません．\n続行しますか？')) {
